c4150bf7d137ace00aaa0d0ce1a13ea42a4572b5
keronimoo <105309554+keronimoo@users.noreply.github.com>
2023-11-01 17:39:06 +0300

    Add files via upload

--- hotel_chatbot.py
+++ hotel_chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from llama_cpp import Llama
+from llama_cpp import Llama , StoppingCriteria , StoppingCriteriaList
 
-llm = Llama(model_path="C:/Users/kerem/Desktop/llmhotel/Model/llama-2-7b-guanaco-qlora.Q4_K_M.gguf")
+llm = Llama(model_path="C:/Users/kerem/Desktop/llmhotel/Model/llama-2-7b-guanaco-qlora.Q4_K_M.gguf" , n_ctx=2048)
 st.title("Assistant")
 
 
@@ -31,14 +31,31 @@ if st.session_state.messages[-1]["role"] != "assistant":
     # Call LLM
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-        
+            
             user_messages = [message["content"] for message in st.session_state.messages if message["role"] == "user"]
             
-            user_input = "\n".join(user_messages)
+            user_input = "\n".join(user_messages).encode("utf-8")
             
-            response = llm(user_input)
-        
-            st.write(response.choices[0])
+           
+            tokens = llm.tokenize(user_input)
+           
 
-    message = {"role": "assistant", "content": response}
+            
+            # response_tokens = llm.generate(tokens, top_k=40, top_p=0.95, temp=1.0, repeat_penalty=1.1,stopping_criteria=None)
+        
+            max_tokens = 512  
+            
+            
+            response_tokens = []
+            for token in llm.generate(tokens, top_k=40, top_p=0.95, temp=1.0, repeat_penalty=1.1):
+                response_tokens.append(token)
+                if len(response_tokens) >= max_tokens:
+                    break
+            
+           
+            response_text = llm.detokenize(response_tokens).decode("utf-8")
+            
+            st.write(response_text)
+
+    message = {"role": "assistant", "content": response_text}
     st.session_state.messages.append(message)
